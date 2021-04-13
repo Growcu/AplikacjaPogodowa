6eac3d3874d5da435a4b4bc58f0b964a2aaa88f8
Growcu <dawid.growiec@interia.pl>
2021-04-13 07:05:58 +0200

    code improvement according to eslint rules

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Aplikacja pogodowa</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './App';
+// eslint-disable-next-line import/no-named-as-default
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+
+reportWebVitals();
