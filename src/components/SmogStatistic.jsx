bb6e3ef3374ed5660d661b7a3655f5810e2caeaf
Growcu <dawid.growiec@interia.pl>
2021-04-14 09:56:49 +0200

    added page wrapper,  style weatherToday component

---
+++ src/components/SmogStatistic.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+import '../styles/SmogStatistic.css';
+
+const SmogStatistic = () => {
+    return (
+        <div className="statistic">
+            <h3>Statistic</h3>
+        </div>
+    );
+};
+export default SmogStatistic;
--- src/components/WeatherToday.jsx
+++ src/components/WeatherToday.jsx
@@ -1,9 +1,29 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 
+import SmogStatistic from './SmogStatistic';
+
+import '../styles/WeatherToday.css';
+
 const WeatherToday = () => {
     return (
-        <h1>Weather Today</h1>
+        <div className="page">
+            <h2 className="title-page">Weather Today</h2>
+            <div className="weather-today-page">
+                <div className="weather-today">
+                    <h1 className="weather-today-title">Miasto</h1>
+                    <ul className="weather-today-list">
+                        <li className="weather-today-list-item">Temp: 23C</li>
+                        <li className="weather-today-list-item">Pressure: 1009hPa</li>
+                        <li className="weather-today-list-item">Humidity: 48%</li>
+                        <li className="weather-today-list-item">Wind: 5km/h</li>
+                        <li className="weather-today-list-item">Temp Max: 27C</li>
+                        <li className="weather-today-list-item">Temp Max: 19C</li>
+                    </ul>
+                </div>
+                <SmogStatistic />
+            </div>
+        </div>
     );
 };
 export default WeatherToday;
