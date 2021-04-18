47de02ce333a498332abed49a9a00fad6ed301e7
Growcu <dawid.growiec@interia.pl>
2021-04-18 14:33:09 +0200

    added more function for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,36 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 import rounderValueHelper from '../helpers/rounderValueHelper';
 
 import '../styles/Card.css';
 
 const Card = ({ data }) => {
+    const [moreIsVisible, setMoreIsVisible] = useState(false);
     const { city, icon } = data;
     const roundedValues = rounderValueHelper(data);
     const [
         temp, pressure, humidity, speed, tempMax, tempMin,
     ] = roundedValues;
+    const listOfInfo = () => (
+        <ul className="card-list">
+            <li className="card-list-item">{`Temp: ${temp}°C`}</li>
+            <li className="card-list-item">{`Pressure: ${pressure}hPa`}</li>
+            <li className="card-list-item">{`Humidity: ${humidity}%`}</li>
+            <li className="card-list-item">{`Wind: ${speed}km/h`}</li>
+            <li className="card-list-item">{`Max: ${tempMax}°C`}</li>
+            <li className="card-list-item">{`Min: ${tempMin}°C`}</li>
+        </ul>
+    );
+    const showMoreHandle = () => setMoreIsVisible(!moreIsVisible);
     return (
         <div className="card">
             <h3 className="card-title">{city}</h3>
             <div className="card-info-wrapper">
-                <ul className="card-list">
-                    <li className="card-list-item">{`Temp: ${temp}°C`}</li>
-                    <li className="card-list-item">{`Pressure: ${pressure}hPa`}</li>
-                    <li className="card-list-item">{`Humidity: ${humidity}%`}</li>
-                    <li className="card-list-item">{`Wind: ${speed}km/h`}</li>
-                    <li className="card-list-item">{`Max: ${tempMax}°C`}</li>
-                    <li className="card-list-item">{`Min: ${tempMin}°C`}</li>
-                </ul>
+                {moreIsVisible ? listOfInfo() : null}
                 <div className="icon"><img src={`./icons/${icon}.png`} alt="ikona" /></div>
             </div>
+            <button type="button" onClick={showMoreHandle}>Wiecej...</button>
         </div>
     );
 };
